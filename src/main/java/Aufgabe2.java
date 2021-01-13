eae3588847f3c7a47fba9995e1a975cf7581cd79
Yanik <bkohly@bbcag.ch>
2021-01-13 16:29:57 +0100

    test commit

---
+++ src/main/java/Aufgabe2.java
@@ -0,0 +1,24 @@
+public class Aufgabe2 {
+    public static void main(String[] args) {
+
+        for (int x = 1; x <= 10; x++){
+
+            System.out.println("Viel Glück!");
+        }
+
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Yanik");
+            i++;
+        }
+
+        int j = 1;
+        do {
+            System.out.println("Yanik");
+            j++;
+        } while (j <= 5);
+
+
+
+    }
+}
